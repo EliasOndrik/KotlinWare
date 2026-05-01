6d7c5827e4bb1134679672e48f55fd1e1d4bce8b
EliasOndrík <elias.ondrik@gmail.com>
2026-05-01 12:11:31 +0200

    Základ výberu hier

---
+++ app/src/main/java/com/example/kotlinware/ui/game/GameSelectionScreen.kt
@@ -0,0 +1,80 @@
+package com.example.kotlinware.ui.game
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun GameSelectionScreen(){
+    Column(
+        modifier = Modifier.background(Color.White)
+    ) {
+        val pagerState = rememberPagerState(pageCount = {
+            6
+        })
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier.fillMaxWidth(),
+        ) { page ->
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Box(
+                    modifier = Modifier.background(Color.Cyan).padding(10.dp)
+                ){
+                    Text("Name")
+                }
+                Box(
+
+                ){
+                    Text("Image")
+                }
+            }
+            
+        }
+        Row(
+            Modifier
+                .wrapContentHeight()
+                .fillMaxWidth()
+                .padding(bottom = 8.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.Bottom
+        ) {
+            repeat(pagerState.pageCount) { iteration ->
+                val color = if (pagerState.currentPage == iteration) Color.DarkGray else Color.LightGray
+                Box(
+                    modifier = Modifier
+                        .padding(2.dp)
+                        .clip(CircleShape)
+                        .background(color)
+                        .size(16.dp)
+                )
+            }
+        }
+
+    }
+}
+
+@Preview
+@Composable
+fun GameSelectionPreview(){
+    GameSelectionScreen()
+}
