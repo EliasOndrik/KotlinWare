d798c821323e212bc62a8fd3ccebe5be792e873b
EliasOndrík <elias.ondrik@gmail.com>
2026-05-02 19:37:15 +0200

    Prvá prechodová aplikácia.

--- app/src/main/java/com/example/kotlinware/MainActivity.kt
+++ app/src/main/java/com/example/kotlinware/MainActivity.kt
@@ -11,7 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import com.example.kotlinware.ui.game.GameSelectionPreview
+import com.example.kotlinware.ui.menu.MenuPreview
+import com.example.kotlinware.ui.navigation.KotlinWareNavHost
 import com.example.kotlinware.ui.theme.KotlinWareTheme
+import com.example.kotlinware.ui.title.TitleScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,12 +26,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             KotlinWareTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
+                windowInsetsController.hide(WindowInsetsCompat.Type.statusBars())
+
+                // Nastaví, aby sa objavil len pri potiahnutí prstom
+                windowInsetsController.systemBarsBehavior =
+                    WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+                KotlinWareNavHost()
             }
         }
     }
