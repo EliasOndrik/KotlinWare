be090203eebf5f43ea5a5c68d84619920079b09d
EliasOndrík <elias.ondrik@gmail.com>
2026-05-02 19:29:35 +0200

    Pridaný obrázok na miesto pre plánovnú hru.

--- app/src/main/java/com/example/kotlinware/ui/game/GameSelectionScreen.kt
+++ app/src/main/java/com/example/kotlinware/ui/game/GameSelectionScreen.kt
@@ -1,10 +1,14 @@
 package com.example.kotlinware.ui.game
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -12,14 +16,20 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.kotlinware.R
 
 @Composable
 fun GameSelectionScreen(){
@@ -45,7 +55,11 @@ fun GameSelectionScreen(){
                 Box(
 
                 ){
-                    Text("Image")
+                    Image(
+                        painter = painterResource(R.drawable.kotlingame),
+                        contentDescription = "",
+                        modifier = Modifier.fillMaxSize(0.7f).border(2.dp,Color.Cyan)
+                    )
                 }
             }
             
@@ -67,6 +81,17 @@ fun GameSelectionScreen(){
                         .background(color)
                         .size(16.dp)
                 )
+            }
+        }
+        Box(
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ){
+            Button(
+                onClick = {},
+                shape = RectangleShape
+            ) {
+                Text("Play")
             }
         }
 
