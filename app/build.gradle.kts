1a3482fd03100a47f4a20f8857640aeb5fd2b6e4
EliasOndrík <elias.ondrik@gmail.com>
2026-05-02 19:25:11 +0200

    Pridaný navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,5 +61,6 @@ dependencies {
     ksp("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+    implementation("androidx.navigation:navigation-compose:2.7.4")
 
 }
