f55d610858e34a02598a253ac9aac649b025b5f2
EliasOndrík <elias.ondrik@gmail.com>
2026-05-02 19:23:47 +0200

    Pridaná náhrada hlavného obrázku

--- app/src/main/java/com/example/kotlinware/ui/title/TitleScreen.kt
+++ app/src/main/java/com/example/kotlinware/ui/title/TitleScreen.kt
@@ -2,16 +2,21 @@ package com.example.kotlinware.ui.title
 
 
 import android.content.res.Configuration
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.kotlinware.R
 
 @Composable
 fun TitleScreen(){
@@ -24,7 +29,15 @@ fun TitleScreen(){
         modifier = Modifier.fillMaxSize().background(Color.White),
         contentAlignment = Alignment.Center
     ){
-        Text("Logo")
+        Box(
+            modifier = Modifier.fillMaxWidth(0.8f).size(200.dp).background(Color.Cyan)
+        ){
+            Image(
+                painter = painterResource(R.drawable.kotlintitle),
+                contentDescription = "",
+                modifier = Modifier.fillMaxSize()
+            )
+        }
     }
 
 }
