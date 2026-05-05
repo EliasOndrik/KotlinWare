eedcc1d76fcf31fe059ecb3d227cf9e8387c3271
EliasOndrík <elias.ondrik@gmail.com>
2026-05-05 11:50:02 +0200

    Vykresľovanie dát z databázy.

--- app/src/main/java/com/example/kotlinware/ui/shop/ShopScreen.kt
+++ app/src/main/java/com/example/kotlinware/ui/shop/ShopScreen.kt
@@ -2,6 +2,7 @@ package com.example.kotlinware.ui.shop
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,8 +10,14 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -18,42 +25,60 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.kotlinware.R
+import com.example.kotlinware.data.Minigame
+import com.example.kotlinware.ui.AppViewModelProvider
 
 @Composable
 fun ShopScreen(
-    viewModel: ShopViewModel = viewModel()
+    viewModel: ShopViewModel = viewModel(factory = AppViewModelProvider.Factory)
 ){
+    val minigames by viewModel.uiState.collectAsStateWithLifecycle()
     Column(
         modifier = Modifier.background(Color.White).padding(20.dp),
     ) {
-        Shelf()
-        Shelf()
-        Shelf()
+        Shelf(minigames)
     }
 }
 
 @Composable
 fun Shelf(
-
+    minigameList: List<Minigame>
 ){
-    Row(
-        modifier = Modifier.fillMaxWidth().background(Color.Cyan),
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2)
     ) {
-        PurchasableGame(0.5f)
-        PurchasableGame(1f)
+        items(
+            items = minigameList,
+
+        ){ minigame ->
+            PurchasableGame(
+                isPurchased = minigame.unlocked,
+                onGameClick = {},
+                name = minigame.name
+            )
+        }
     }
+
+
 }
 
 @Composable
 fun PurchasableGame(
-    fraction: Float,
+    name: String = "",
     isPurchased: Boolean = false,
+    onGameClick: ()->Unit,
     price: Int = 40,
 ){
     Box(
-        modifier = Modifier.fillMaxWidth(fraction).size(200.dp).padding(10.dp).background(Color.Blue),
+        modifier = Modifier
+            .fillMaxWidth()
+            .size(200.dp)
+            .padding(10.dp)
+            .background(Color.Blue)
+            .clickable{},
         contentAlignment = Alignment.Center
     ) {
         Column(
@@ -85,5 +110,10 @@ fun PurchasableGame(
 @Preview
 @Composable
 fun ShopPreview(){
-    ShopScreen()
+    Shelf(
+        listOf(Minigame(0,"shit",0,false),
+            Minigame(0,"shit",0,true),
+            Minigame(0,"shit",0,false),
+            Minigame(0,"shit",0,false))
+    )
 }
--- app/src/main/java/com/example/kotlinware/ui/shop/ShopViewModel.kt
+++ app/src/main/java/com/example/kotlinware/ui/shop/ShopViewModel.kt
@@ -1,7 +1,20 @@
 package com.example.kotlinware.ui.shop
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.kotlinware.data.GameInterfaceRepository
+import com.example.kotlinware.data.Minigame
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
 
-class ShopViewModel : ViewModel() {
+class ShopViewModel(
+    gameRepository: GameInterfaceRepository
+) : ViewModel() {
 
+    val uiState : StateFlow<List<Minigame>> = gameRepository.getAllMinigames().stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000),
+        initialValue = listOf()
+    )
 }
