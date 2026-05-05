90b23498b2d85f84ed9e41a75c81048da4c704ec
EliasOndrík <elias.ondrik@gmail.com>
2026-05-05 11:42:58 +0200

    Upravené nastavenia aplikácie pre fungovania Room.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,9 +57,9 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 
-    implementation("androidx.room:room-runtime:2.6.1")
-    ksp("androidx.room:room-compiler:2.6.1")
-    implementation("androidx.room:room-ktx:2.6.1")
+    implementation("androidx.room:room-runtime:2.7.0-alpha13")
+    ksp("androidx.room:room-compiler:2.7.0-alpha13")
+    implementation("androidx.room:room-ktx:2.7.0-alpha13")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
     implementation("androidx.navigation:navigation-compose:2.7.4")
 
