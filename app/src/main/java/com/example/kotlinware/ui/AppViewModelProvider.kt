5e47d3c4f1aa2e22f6ba3003c011a811ec4156d7
EliasOndrík <elias.ondrik@gmail.com>
2026-05-05 11:47:47 +0200

    Model poskytovatel

---
+++ app/src/main/java/com/example/kotlinware/ui/AppViewModelProvider.kt
@@ -0,0 +1,27 @@
+package com.example.kotlinware.ui
+
+import com.example.kotlinware.KotlinWareApplication
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.kotlinware.ui.shop.ShopViewModel
+
+/**
+ * Provides Factory to create instance of ViewModel for the entire Inventory app
+ */
+object AppViewModelProvider {
+    val Factory = viewModelFactory {
+        initializer {
+            ShopViewModel(kotlinWareApplication().container.gameRepository)
+        }
+
+    }
+}
+
+/**
+ * Extension function to queries for [Application] object and returns an instance of
+ * [InventoryApplication].
+ */
+fun CreationExtras.kotlinWareApplication(): KotlinWareApplication =
+    (this[AndroidViewModelFactory.APPLICATION_KEY] as KotlinWareApplication)
