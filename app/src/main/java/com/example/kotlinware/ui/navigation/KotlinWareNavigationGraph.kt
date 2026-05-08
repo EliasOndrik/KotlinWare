ee4a2c544c128ddb67be4a5c25bd2de1bde1b43a
EliasOndrík <elias.ondrik@gmail.com>
2026-05-08 16:20:18 +0200

    Pridaná databáza a pridaný základ spustenia hry.

--- app/src/main/java/com/example/kotlinware/ui/navigation/KotlinWareNavigationGraph.kt
+++ app/src/main/java/com/example/kotlinware/ui/navigation/KotlinWareNavigationGraph.kt
@@ -4,17 +4,24 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navigation
+import com.example.kotlinware.ui.AppViewModelProvider
 import com.example.kotlinware.ui.game.GameSelectionScreen
 import com.example.kotlinware.ui.menu.BottomMenuBar
 import com.example.kotlinware.ui.menu.MenuDestination
 import com.example.kotlinware.ui.menu.TopMenuBar
+import com.example.kotlinware.ui.minigames.IntermissionScreen
+import com.example.kotlinware.ui.minigames.tapping.BallonPopScreen
+import com.example.kotlinware.ui.minigames.tapping.TappingGamesScreen
 import com.example.kotlinware.ui.shop.ShopScreen
 import com.example.kotlinware.ui.title.TitleScreen
 
@@ -25,11 +32,12 @@ enum class AppDestination{
 
 @Composable
 fun KotlinWareNavHost(
-    viewModel: NavigationViewModel = viewModel(),
+    viewModel: NavigationViewModel = viewModel(factory = AppViewModelProvider.Factory),
     navHostController: NavHostController = rememberNavController()
 ){
+    val playerState by viewModel.playerState.collectAsStateWithLifecycle()
     Scaffold(
-        topBar = { TopMenuBar() },
+        topBar = { TopMenuBar(coins = playerState.money) },
         bottomBar = { BottomMenuBar(
             {navHostController.navigate(MenuDestination.TITLE.name)},
             {navHostController.navigate(MenuDestination.GAMES.name)},
@@ -55,12 +63,26 @@ fun KotlinWareNavHost(
                     composable(
                         route = MenuDestination.GAMES.name
                     ){
-                        GameSelectionScreen()
+                        GameSelectionScreen(onPlayClick = {
+                            name->
+                            navHostController.navigate("tap")
+
+                        })
                     }
                     composable(
                         route = MenuDestination.SHOP.name
                     ){
                         ShopScreen()
+                    }
+                }
+                navigation(
+                    startDestination = "tap",
+                    route = AppDestination.GAME.name
+                ){
+                    composable(
+                        route = "tap"
+                    ){
+                        TappingGamesScreen()
                     }
                 }
             }
--- app/src/main/java/com/example/kotlinware/ui/navigation/NavigationViewModel.kt
+++ app/src/main/java/com/example/kotlinware/ui/navigation/NavigationViewModel.kt
@@ -1,7 +1,23 @@
 package com.example.kotlinware.ui.navigation
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.kotlinware.data.GameInterfaceRepository
+import com.example.kotlinware.data.Player
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
 
-class NavigationViewModel : ViewModel() {
-
+class NavigationViewModel(
+    gameRepository: GameInterfaceRepository
+) : ViewModel() {
+    val playerState: StateFlow<Player> = gameRepository.getPlayer()
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = Player(0,0)
+        )
 }
