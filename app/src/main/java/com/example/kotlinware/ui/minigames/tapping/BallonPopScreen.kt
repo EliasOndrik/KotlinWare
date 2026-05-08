add671788ea7823540e8b92e170dd23fdf2899da
EliasOndrík <elias.ondrik@gmail.com>
2026-05-08 16:18:39 +0200

    Pridaná minihra s vybuchovaním balónov.

---
+++ app/src/main/java/com/example/kotlinware/ui/minigames/tapping/BallonPopScreen.kt
@@ -0,0 +1,74 @@
+package com.example.kotlinware.ui.minigames.tapping
+
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.drawscope.scale
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+@Composable
+fun BallonPopScreen(
+    onGameSuccess: ()-> Unit ,
+    viewModel: BallonPopViewModel = viewModel()
+){
+    val ballons by viewModel.ballons.collectAsState()
+    Box(
+        modifier = Modifier.fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ){
+        Text("Pop", fontSize = 40.sp)
+    }
+    Canvas(
+        modifier = Modifier.fillMaxSize().pointerInput(Unit){
+            detectTapGestures { offset ->
+                viewModel.checkBallonsCollision(offset - Offset(size.width/2f,size.height/2f))
+                if (viewModel.areBallonsPopped()){
+                    onGameSuccess()
+                }
+            }
+        }
+    ) {
+        val screenWidth = size.width
+        val screenHeight = size.height
+        scale(
+            scaleX = 0.8f,
+            scaleY = 1f
+        ){
+            ballons.asReversed().forEach { ballon ->
+                if (!ballon.popped){
+                    drawCircle(
+                        color = ballon.color,
+                        radius = 200f,
+                        center = Offset(screenWidth/2 + ballon.x,
+                            screenHeight/2 + ballon.y)
+                    )
+                }
+
+            }
+
+
+        }
+
+
+    }
+}
+
+@Preview
+@Composable
+fun BallonPopPreview(){
+    BallonPopScreen({})
+}
---
+++ app/src/main/java/com/example/kotlinware/ui/minigames/tapping/BallonPopViewModel.kt
@@ -0,0 +1,62 @@
+package com.example.kotlinware.ui.minigames.tapping
+
+
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.count
+import kotlinx.coroutines.flow.update
+import kotlin.random.Random
+
+class BallonPopViewModel: ViewModel() {
+
+    private val _ballons = MutableStateFlow(listOf(
+        Ballon(0f, 120f, BallonColor.BLUE.color,false),
+        Ballon(-320f, -80f, BallonColor.RED.color,false),
+        Ballon(320f, 200f, BallonColor.RED.color,false),
+        Ballon(-300f, 200f, BallonColor.YELLOW.color,false),
+        Ballon(0f, -100f, BallonColor.BLUE.color,false),
+        Ballon(300f, -120f, BallonColor.GREEN.color,false),
+    ))
+    val ballons = _ballons.asStateFlow()
+    fun checkBallonsCollision(offset: Offset){
+        _ballons.update { currentList ->
+            var wasPopped = false
+            currentList.map { ballon ->
+
+                val distance = (offset - Offset(ballon.x,ballon.y)).getDistance()
+                if (wasPopped){
+                    ballon
+                } else if (distance <= 200f){
+                    wasPopped = true
+                    ballon.copy(popped = true)
+                } else{
+                    ballon
+                }
+            }
+        }
+    }
+    fun areBallonsPopped(): Boolean{
+        var arePopped = true
+        for (i in 0..<_ballons.value.size){
+            arePopped = arePopped && _ballons.value[i].popped
+        }
+        return arePopped
+    }
+}
+data class Ballon(
+    val x: Float,
+    val y: Float,
+    val color: Color,
+    val popped: Boolean
+)
+enum class BallonColor(
+    val color: Color
+){
+    RED(Color.Red),
+    GREEN(Color.Green),
+    BLUE(Color.Blue),
+    YELLOW(Color.Yellow)
+}
