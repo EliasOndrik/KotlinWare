efb5a1dd1657fe9f14e01db8a1f6b0d90d9300c0
EliasOndrík <elias.ondrik@gmail.com>
2026-05-09 16:29:09 +0200

    Upravené farby a theme.

--- app/src/main/java/com/example/kotlinware/ui/theme/Color.kt
+++ app/src/main/java/com/example/kotlinware/ui/theme/Color.kt
@@ -2,10 +2,11 @@ package com.example.kotlinware.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+
+val Yellow = Color(0xfffadda2)
+val DarkBlue = Color(0xff6b61ff)
+val SoftBlue = Color(0xff66a1ff)
+val SoftGreen = Color(0xff8ecde6)
+val BlueishWhite = Color(0xfff0eff4)
+val Grey = Color(0xff332f35)
