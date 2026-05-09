880b1dac45392e5ab6a7ff4d21c68abb2ff6ff5e
EliasOndrík <elias.ondrik@gmail.com>
2026-05-09 16:32:00 +0200

    Zmenený dizaj a pridaný spôsob kupovania hier.

--- app/src/main/java/com/example/kotlinware/ui/shop/ShopScreen.kt
+++ app/src/main/java/com/example/kotlinware/ui/shop/ShopScreen.kt
@@ -1,27 +1,37 @@
 package com.example.kotlinware.ui.shop
 
+import android.content.Context
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.drawscope.scale
+import androidx.compose.ui.graphics.drawscope.translate
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -30,6 +40,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.kotlinware.R
 import com.example.kotlinware.data.Minigame
 import com.example.kotlinware.ui.AppViewModelProvider
+import com.example.kotlinware.ui.theme.KotlinWareTheme
 
 @Composable
 fun ShopScreen(
@@ -37,70 +48,107 @@ fun ShopScreen(
 ){
     val minigames by viewModel.uiState.collectAsStateWithLifecycle()
     Column(
-        modifier = Modifier.background(Color.White).padding(20.dp),
+        modifier = Modifier.padding(20.dp)
+
     ) {
-        Shelf(minigames)
+        Shelf(minigameList = minigames, onGameClick = {viewModel.onGameClick(it)})
     }
+
 }
 
 @Composable
 fun Shelf(
-    minigameList: List<Minigame>
+    minigameList: List<Minigame>,
+    onGameClick: (Minigame)->Unit,
 ){
+    val context = LocalContext.current
+    val imageBitmap = ImageBitmap.imageResource(R.drawable.shelf)
+
+    val gridState = rememberLazyGridState()
+    val totalScrollOffset by remember {
+        derivedStateOf {
+            val layoutInfo = gridState.layoutInfo
+            val visibleItems = layoutInfo.visibleItemsInfo
+            if (visibleItems.isEmpty()) {
+                0f
+            } else {
+                visibleItems.first().offset.y
+            }
+        }
+    }
     LazyVerticalGrid(
-        columns = GridCells.Fixed(2)
+        columns = GridCells.Fixed(2),
+        state = gridState,
+        modifier = Modifier.drawBehind{
+            scale(scaleX = size.width/imageBitmap.width, scaleY = 1f){
+                translate(left = 0f, top = totalScrollOffset.toFloat()) {
+                    for (i in 0..<minigameList.count()/2){
+                        drawImage(imageBitmap,
+                          topLeft = Offset(((size.width-imageBitmap.width)/2),(i*200+180).dp.toPx())
+                        )
+                    }
+                }
+            }
+        }
+
     ) {
         items(
             items = minigameList,
 
         ){ minigame ->
             PurchasableGame(
-                isPurchased = minigame.unlocked,
-                onGameClick = {},
-                name = minigame.name
+                onGameClick = onGameClick,
+                minigame = minigame,
+                context = context
             )
         }
     }
-
-
 }
 
 @Composable
 fun PurchasableGame(
-    name: String = "",
-    isPurchased: Boolean = false,
-    onGameClick: ()->Unit,
+    minigame: Minigame = Minigame(0,"",0,false),
+    onGameClick: (Minigame)->Unit,
     price: Int = 40,
+    context: Context = LocalContext.current
 ){
+
+    val imageId = remember(minigame.name) {
+        context.resources.getIdentifier(
+            minigame.name,
+            "drawable",
+            context.packageName
+        )
+    }
     Box(
         modifier = Modifier
             .fillMaxWidth()
             .size(200.dp)
             .padding(10.dp)
-            .background(Color.Blue)
-            .clickable{},
-        contentAlignment = Alignment.Center
+            .clickable{onGameClick(minigame)},
+        contentAlignment = Alignment.BottomCenter
     ) {
         Column(
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            if(!isPurchased){
+            if(!minigame.unlocked){
                 Box(
-                    modifier = Modifier.fillMaxHeight(0.8f)
+                    modifier = Modifier.fillMaxHeight(0.7f)
                 ){
                     Image(
-                        painter = painterResource(R.drawable.kotlingame),
+                        painter = painterResource(imageId),
                         contentDescription = ""
                     )
                 }
             }
             Box(
-                modifier = Modifier.background(Color.White).padding(10.dp)
+                modifier = Modifier.background(MaterialTheme.colorScheme.tertiary).padding(10.dp)
             ){
-                if (isPurchased){
-                    Text("Sold")
+                if (minigame.unlocked){
+                    Text("Sold",
+                        color = Color.Black)
                 } else {
-                    Text("Pay $price coins")
+                    Text("Pay $price coins",color = Color.Black)
                 }
             }
         }
@@ -110,10 +158,12 @@ fun PurchasableGame(
 @Preview
 @Composable
 fun ShopPreview(){
-    Shelf(
-        listOf(Minigame(0,"shit",0,false),
-            Minigame(0,"shit",0,true),
-            Minigame(0,"shit",0,false),
-            Minigame(0,"shit",0,false))
-    )
+    KotlinWareTheme {
+        Shelf(
+            listOf(Minigame(0,"tapping",0,false),
+                Minigame(0,"tapping",0,true),
+                Minigame(0,"rotating",0,false),
+                Minigame(0,"tapping",0,false))
+        ) {}
+    }
 }
--- app/src/main/java/com/example/kotlinware/ui/shop/ShopViewModel.kt
+++ app/src/main/java/com/example/kotlinware/ui/shop/ShopViewModel.kt
@@ -4,17 +4,37 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.kotlinware.data.GameInterfaceRepository
 import com.example.kotlinware.data.Minigame
+import com.example.kotlinware.data.Player
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 class ShopViewModel(
-    gameRepository: GameInterfaceRepository
+    private val gameRepository: GameInterfaceRepository
 ) : ViewModel() {
-
+    val player: StateFlow<Player> = gameRepository.getPlayer().stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000),
+        initialValue = Player(0,0)
+    )
     val uiState : StateFlow<List<Minigame>> = gameRepository.getAllMinigames().stateIn(
         scope = viewModelScope,
         started = SharingStarted.WhileSubscribed(5000),
         initialValue = listOf()
     )
+    fun onGameClick(minigame: Minigame){
+        if (minigame.unlocked){
+            return
+        }
+        if (player.value.money< 40){
+            return
+        }
+        val currentPlayer = player.value
+        viewModelScope.launch {
+            gameRepository.updateMinigame(minigame.copy(unlocked = true))
+            val updatedPlayer = currentPlayer.copy(money = currentPlayer.money - 40)
+            gameRepository.updatePlayer(updatedPlayer)
+        }
+    }
 }
