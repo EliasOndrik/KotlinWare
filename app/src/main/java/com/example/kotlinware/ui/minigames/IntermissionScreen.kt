2c4ced5111685f901be1b4534f04135636b3238e
EliasOndrík <elias.ondrik@gmail.com>
2026-05-11 18:57:29 +0200

    Menšie úpravy textu.

--- app/src/main/java/com/example/kotlinware/ui/minigames/IntermissionScreen.kt
+++ app/src/main/java/com/example/kotlinware/ui/minigames/IntermissionScreen.kt
@@ -1,16 +1,20 @@
 package com.example.kotlinware.ui.minigames
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.drawText
 import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
@@ -40,7 +44,7 @@ fun IntermissionScreen(
             text = score.toString(),
             textMeasurer = textMeasurer,
             topLeft = Offset((canvasWidth-measuredText.size.width)/2,(3*canvasHeight/2-measuredText.size.height)/2),
-            style = TextStyle(fontSize = 100.sp)
+            style = TextStyle(fontSize = 100.sp, color = Color.Cyan)
         )
     }
 }
