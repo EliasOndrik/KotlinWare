d459c30415475dbb97c657ce57618a04b04bd2cc
EliasOndrík <elias.ondrik@gmail.com>
2026-05-12 13:47:52 +0200

    Pridaný GameOver screen

--- app/src/main/java/com/example/kotlinware/ui/minigames/IntermissionScreen.kt
+++ app/src/main/java/com/example/kotlinware/ui/minigames/IntermissionScreen.kt
@@ -1,18 +1,32 @@
 package com.example.kotlinware.ui.minigames
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.drawText
 import androidx.compose.ui.text.rememberTextMeasurer
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -38,8 +52,7 @@ fun IntermissionScreen(
         val measuredText = textMeasurer.measure(
             text = score.toString(),
             style = TextStyle(fontSize = 100.sp)
-
-            )
+        )
         drawText(
             text = score.toString(),
             textMeasurer = textMeasurer,
@@ -48,9 +61,56 @@ fun IntermissionScreen(
         )
     }
 }
+@Composable
+fun GameOverScreen(
+    onQuit:()->Unit,
+    onRetry:()->Unit,
+    score: Int
+){
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ){
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                "Final Score",
+                fontSize = 50.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(bottom = 20.dp)
+            )
+            Text(
+                score.toString(),
+                fontSize = 40.sp,
+                modifier = Modifier.padding(bottom = 20.dp)
+            )
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Button(
+                    onClick = {onRetry()},
+                    shape = RectangleShape,
+                    border = BorderStroke(2.dp, MaterialTheme.colorScheme.tertiary)
+                ) {
+                    Text("Retry")
+                }
+                Button(
+                    onClick = {onQuit()},
+                    shape = RectangleShape,
+                    border = BorderStroke(2.dp, MaterialTheme.colorScheme.tertiary)
+                ) {
+                    Text("Quit")
+                }
+            }
+        }
+    }
+}
 
 @Preview
 @Composable
 fun IntermissionScreenPreview(){
-    IntermissionScreen()
+    IntermissionScreen(0,0)
+    GameOverScreen({},{},0)
 }
