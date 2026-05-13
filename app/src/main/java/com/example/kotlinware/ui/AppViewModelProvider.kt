b7eb546cff9f525b5666efca6945eeae502b3586
EliasOndrík <elias.ondrik@gmail.com>
2026-05-13 16:56:10 +0200

    Pridaný inicializér pre manažéra otáčania mobilu.

--- app/src/main/java/com/example/kotlinware/ui/AppViewModelProvider.kt
+++ app/src/main/java/com/example/kotlinware/ui/AppViewModelProvider.kt
@@ -1,5 +1,8 @@
 package com.example.kotlinware.ui
 
+import android.app.Application
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.ViewModelProvider
 import com.example.kotlinware.KotlinWareApplication
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory
 import androidx.lifecycle.viewmodel.CreationExtras
@@ -7,6 +10,8 @@ import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.kotlinware.ui.game.GameSelectionViewModel
 import com.example.kotlinware.ui.minigames.AbstractManagerViewModel
+import com.example.kotlinware.ui.minigames.rotating.RotatingGamesScreen
+import com.example.kotlinware.ui.minigames.rotating.RotatingGamesViewModel
 import com.example.kotlinware.ui.minigames.tapping.TappingGamesViewModel
 import com.example.kotlinware.ui.navigation.NavigationViewModel
 import com.example.kotlinware.ui.shop.ShopViewModel
@@ -27,6 +32,11 @@ object AppViewModelProvider {
         }
         initializer {
             TappingGamesViewModel(kotlinWareApplication().container.gameRepository)
+        }
+        initializer {
+            val application = this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as Application
+            RotatingGamesViewModel(application,kotlinWareApplication().container.gameRepository)
+
         }
 
     }
