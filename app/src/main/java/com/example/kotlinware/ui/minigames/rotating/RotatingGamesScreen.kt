27b0a04130c4627717d215bf1efabf81f41b9bf2
EliasOndrík <elias.ondrik@gmail.com>
2026-05-13 16:54:30 +0200

    Pridaný nový manažer pre hry s otáčaním mobilu.

---
+++ app/src/main/java/com/example/kotlinware/ui/minigames/rotating/RotatingGamesScreen.kt
@@ -0,0 +1,69 @@
+package com.example.kotlinware.ui.minigames.rotating
+
+import android.app.Activity
+import android.content.pm.ActivityInfo
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.withFrameMillis
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.kotlinware.ui.AppViewModelProvider
+import com.example.kotlinware.ui.minigames.GameOverScreen
+import com.example.kotlinware.ui.minigames.IntermissionScreen
+import com.example.kotlinware.ui.minigames.MinigameType
+
+@Composable
+fun RotatingGamesScreen(
+    onQuit: ()-> Unit,
+    viewModel: RotatingGamesViewModel = viewModel(factory = AppViewModelProvider.Factory)
+) {
+    val currentMinigame by viewModel.currentMinigame.collectAsStateWithLifecycle()
+    val gameProgress by viewModel.gameProgress.collectAsStateWithLifecycle()
+    val deltaTime by viewModel.timerMillis.collectAsStateWithLifecycle()
+    val rotations by viewModel.rotationData.collectAsStateWithLifecycle()
+    val context = LocalContext.current
+    DisposableEffect(Unit) {
+        val activity = context as? Activity
+        activity?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+
+        viewModel.startListening()
+        onDispose {
+            viewModel.stopListening()
+            activity?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+        }
+    }
+    LaunchedEffect(Unit) {
+        while (true){
+            withFrameMillis { frameTimeMillis ->
+                viewModel.updateTimeMillis(frameTimeMillis)
+            }
+        }
+    }
+    when (currentMinigame){
+        MinigameType.TRANSITION -> {
+            if (gameProgress.lives> 0){
+                IntermissionScreen(gameProgress.lives,gameProgress.score)
+            }else{
+                GameOverScreen(
+                    onQuit = onQuit,
+                    onRetry = {viewModel.onGameRetry()},
+                    score = gameProgress.score
+                )
+            }
+        }
+
+        MinigameType.WAVEGOODBYE -> {
+            WaveGoodbyeScreen(
+                onSuccess = {viewModel.onGameSuccess()},
+                rotations,
+                viewModel.waveGoodbyeViewModel
+            )
+        }
+
+        else -> {}
+    }
+
+}
---
+++ app/src/main/java/com/example/kotlinware/ui/minigames/rotating/RotatingGamesViewModel.kt
@@ -0,0 +1,162 @@
+package com.example.kotlinware.ui.minigames.rotating
+
+import android.app.Application
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.kotlinware.data.GameInterfaceRepository
+import com.example.kotlinware.ui.minigames.GameProgress
+import com.example.kotlinware.ui.minigames.MinigameType
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import kotlin.random.Random
+
+class RotatingGamesViewModel(
+    application: Application,
+    val gameRepository: GameInterfaceRepository
+) : AndroidViewModel(application) {
+    val waveGoodbyeViewModel = WaveGoodbyeViewModel()
+    private val sensorManager = application.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    private val rotationSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GAME_ROTATION_VECTOR)
+
+    private val _rotationData = MutableStateFlow(FloatArray(3))
+    val rotationData = _rotationData.asStateFlow()
+    private val sensorListener = object : SensorEventListener {
+        override fun onSensorChanged(event: SensorEvent?) {
+            if (event?.sensor?.type == Sensor.TYPE_GAME_ROTATION_VECTOR) {
+                val rotationMatrix = FloatArray(9)
+                SensorManager.getRotationMatrixFromVector(rotationMatrix, event.values)
+                val orientation = FloatArray(3)
+                SensorManager.getOrientation(rotationMatrix, orientation)
+
+                _rotationData.update { orientation }
+            }
+        }
+        override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+    }
+
+    fun startListening() {
+        rotationSensor?.let {
+            sensorManager.registerListener(sensorListener, it, SensorManager.SENSOR_DELAY_UI)
+        }
+    }
+
+    fun stopListening() {
+        sensorManager.unregisterListener(sensorListener)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        stopListening()
+    }
+// Nedokáže dediť od viewModel a abstractManager naraz.
+    val minigameName: String = "rotating"
+    private val _gameProgress = MutableStateFlow(GameProgress())
+    val gameProgress = _gameProgress.asStateFlow()
+    private val _currentMinigame = MutableStateFlow(MinigameType.TRANSITION)
+    val currentMinigame = _currentMinigame.asStateFlow()
+    private val _previousTimeMillis = MutableStateFlow(0L)
+    val previousTimeMillis = _previousTimeMillis.asStateFlow()
+    private val _timerMillis = MutableStateFlow(0L)
+    val timerMillis = _timerMillis.asStateFlow()
+    private var success = false
+    private var saved = false
+
+    fun switchMinigameType(type: MinigameType){
+        _currentMinigame.update { type }
+    }
+    fun updateTimeMillis(time: Long){
+        if (_gameProgress.value.lives<=0){
+            if (!saved){
+                saveProgress(minigameName)
+                saved = true
+            }
+            return
+        }
+        if (_previousTimeMillis.value <= 0L){
+            initializeTimer(time)
+        }
+        _timerMillis.update { it - (time - _previousTimeMillis.value) }
+        _previousTimeMillis.update { time }
+        if (_timerMillis.value<= 0L){
+            if (_currentMinigame.value == MinigameType.TRANSITION){
+                pickRandomMinigame()
+                resetMinigame()
+
+            } else {
+                onGameEnded()
+            }
+            resetTimer(_currentMinigame.value.time)
+        }
+    }
+    fun resetTimer(time: Long){
+        _timerMillis.value = time
+    }
+    fun pickRandomMinigame() {
+        val randomIndex = 0//Random.nextInt(5)
+        when(randomIndex){
+            0 -> {switchMinigameType(MinigameType.WAVEGOODBYE )}
+            //1 -> {switchMinigameType(MinigameType.CATTAP)}
+            //2 -> {switchMinigameType(MinigameType.PENWASTE)}
+            //3 -> {switchMinigameType(MinigameType.CORRECTORDER)}
+            else -> {}
+        }
+    }
+
+    fun onGameSuccess(){
+        success = true
+        resetTimer(_timerMillis.value.coerceAtMost(1000L))
+    }
+    fun onGameFail(){
+        success = false
+        resetTimer(_timerMillis.value.coerceAtMost(1000L))
+    }
+    fun onGameRetry(){
+        initializeTimer(0L)
+        _gameProgress.update { GameProgress() }
+        _currentMinigame.value = MinigameType.TRANSITION
+        success = false
+        saved = false
+    }
+
+    fun onGameEnded(){
+        if (success){
+            _gameProgress.update { it.copy(score = it.score + 1) }
+        } else {
+            _gameProgress.update { it.copy(lives = it.lives - 1, score = it.score + 1) }
+        }
+        _currentMinigame.value = MinigameType.TRANSITION
+        success = false
+    }
+    private fun initializeTimer(time: Long){
+        _previousTimeMillis.update { time }
+    }
+    private fun saveProgress(minigameName: String){
+        if(_gameProgress.value.lives>0){
+            return
+        }
+        viewModelScope.launch {
+            val currentMinigame = gameRepository.getMinigameByName(minigameName).first()
+            if (_gameProgress.value.score > currentMinigame.score){
+                val updateMinigame = currentMinigame.copy(score = _gameProgress.value.score)
+                gameRepository.updateMinigame(updateMinigame)
+            }
+            val currentPlayer = gameRepository.getPlayer().first()
+            val updatePlayer = currentPlayer.copy(money = currentPlayer.money + _gameProgress.value.score)
+            gameRepository.updatePlayer(updatePlayer)
+        }
+    }
+    fun resetMinigame(){
+        when(currentMinigame.value){
+            MinigameType.WAVEGOODBYE -> {waveGoodbyeViewModel.resetMinigame()}
+            else -> {}
+        }
+    }
+}
