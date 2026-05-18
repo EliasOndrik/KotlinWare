a360a79d40d6810ce48a7039404b9bd3cb82e2ea
EliasOndrík <elias.ondrik@gmail.com>
2026-05-18 13:46:30 +0200

    Opravená chyba s nakupovaním hier.

--- app/src/main/java/com/example/kotlinware/ui/shop/ShopViewModel.kt
+++ app/src/main/java/com/example/kotlinware/ui/shop/ShopViewModel.kt
@@ -7,17 +7,14 @@ import com.example.kotlinware.data.Minigame
 import com.example.kotlinware.data.Player
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 class ShopViewModel(
     private val gameRepository: GameInterfaceRepository
 ) : ViewModel() {
-    val player: StateFlow<Player> = gameRepository.getPlayer().stateIn(
-        scope = viewModelScope,
-        started = SharingStarted.WhileSubscribed(5000),
-        initialValue = Player(0,0)
-    )
+    
     val uiState : StateFlow<List<Minigame>> = gameRepository.getAllMinigames().stateIn(
         scope = viewModelScope,
         started = SharingStarted.WhileSubscribed(5000),
@@ -27,14 +24,13 @@ class ShopViewModel(
         if (minigame.unlocked){
             return
         }
-        if (player.value.money< 40){
-            return
-        }
-        val currentPlayer = player.value
         viewModelScope.launch {
-            gameRepository.updateMinigame(minigame.copy(unlocked = true))
-            val updatedPlayer = currentPlayer.copy(money = currentPlayer.money - 40)
-            gameRepository.updatePlayer(updatedPlayer)
+            val currentPlayer = gameRepository.getPlayer().first()
+            if (currentPlayer.money >= 40){
+                gameRepository.updateMinigame(minigame.copy(unlocked = true))
+                val updatedPlayer = currentPlayer.copy(money = currentPlayer.money - 40)
+                gameRepository.updatePlayer(updatedPlayer)
+            }
         }
     }
 }
